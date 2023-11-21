9aab1a1bdb35a68a178bd1a5dd01992c6f7b9f88
yein <tbc07061@naver.com>
2023-11-21 14:38:28 +0900

    villager card prev/next

--- animal_crossing/app/src/main/java/com/ssafy/animal_crossing_nh_guide/villager/VillagerFragmentViewModel.kt
+++ animal_crossing/app/src/main/java/com/ssafy/animal_crossing_nh_guide/villager/VillagerFragmentViewModel.kt
@@ -43,10 +43,8 @@ class VillagerFragmentViewModel: ViewModel() {
     }
 
     private val _filePath = MutableLiveData<String>("")
-
     val filePath : LiveData<String>
         get() = _filePath
-
     fun getFilePath(name: String) {
         _filePath.value = ""
         viewModelScope.launch {
@@ -61,4 +59,41 @@ class VillagerFragmentViewModel: ViewModel() {
             }
         }
     }
+
+    private val _filePath1 = MutableLiveData<String>("")
+    val filePath1 : LiveData<String>
+        get() = _filePath1
+    fun getFilePath1(name: String) {
+        _filePath1.value = ""
+        viewModelScope.launch {
+            var result:List<AcnhVillager>
+            try {
+                result = RetrofitUtil.acnhService.getAcnhVillager(name)
+            }catch (e: Exception){
+                result = listOf()
+            }
+            if(result.size != 0){
+                _filePath1.value = result[0].image_url
+            }
+        }
+    }
+
+    private val _filePath2 = MutableLiveData<String>("")
+    val filePath2 : LiveData<String>
+        get() = _filePath2
+    fun getFilePath2(name: String) {
+        _filePath2.value = ""
+        viewModelScope.launch {
+            var result:List<AcnhVillager>
+            try {
+                result = RetrofitUtil.acnhService.getAcnhVillager(name)
+            }catch (e: Exception){
+                result = listOf()
+            }
+            if(result.size != 0){
+                _filePath2.value = result[0].image_url
+            }
+        }
+    }
+
 }
