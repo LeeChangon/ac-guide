4be29d989eaccdb773438bbfcdf74220c1ccda27
yein <tbc07061@naver.com>
2023-11-22 14:37:51 +0900

    카드 배경 엉엉

--- animal_crossing/app/src/main/java/com/ssafy/animal_crossing_nh_guide/villager/VillagerDetailFragment2.kt
+++ animal_crossing/app/src/main/java/com/ssafy/animal_crossing_nh_guide/villager/VillagerDetailFragment2.kt
@@ -1,13 +1,14 @@
 package com.ssafy.animal_crossing_nh_guide.critterpedia.seacreature
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.ssafy.animal_crossing_nh_guide.R
 import com.ssafy.animal_crossing_nh_guide.config.BaseFragment
 import com.ssafy.animal_crossing_nh_guide.databinding.FragmentVillagerDetail2Binding
 import com.ssafy.animal_crossing_nh_guide.models.villager.Villager
 
-
+private const val TAG = "VillagerDetailFragment2_싸피"
 class VillagerDetailFragment2(var filepath: String, var villager: Villager) : BaseFragment<FragmentVillagerDetail2Binding>(
     FragmentVillagerDetail2Binding::bind, R.layout.fragment_villager_detail2
 ){
@@ -16,6 +17,10 @@ class VillagerDetailFragment2(var filepath: String, var villager: Villager) : Ba
         super.onViewCreated(view, savedInstanceState)
         binding.filepath = filepath
         view.cameraDistance = 8000*requireContext().resources.displayMetrics.density
+        val birthday = villager.birthday.split("*", "/")
+        binding.bmonth = birthday[1]
+        binding.bday = birthday[0]
+        Log.d(TAG, "onViewCreated: ${birthday}")
         binding.villager = villager
 
     }
